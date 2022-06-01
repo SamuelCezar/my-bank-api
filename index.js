0a4b82b490e36b880390b19a16a7fd8fda64a05d
Samuel <samuelcezar@outlook.com>
2022-06-01 10:07:12 -0300

    API Adjusts and CORS policy

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
 import express from "express";
-import accountsRouter from "./routes/accounts.js"
+import accountsRouter from "./routes/accounts.js";
 import { promises as fs } from "fs";
+import cors from "cors";
 
 global.fileName = "accounts.json";
 
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 app.use("/account", accountsRouter);
 
 app.listen(3000, async () => {
-    try {
-        await fs.readFile(global.fileName);
-        console.log("API Started!");
-    } catch (err) {
-        const initialJson = {
-            nextId: 1,
-            accounts: []
-        }
-        fs.writeFile(global.fileName, JSON.stringify(initialJson)).then(() => {
-            console.log("API Started and file has been created!")
-        }).catch(err => {
-            console.log(err);
-        });
-    }
-
-})
+  try {
+    await fs.readFile(global.fileName);
+    console.log("API Started!");
+  } catch (err) {
+    const initialJson = {
+      nextId: 1,
+      accounts: [],
+    };
+    fs.writeFile(global.fileName, JSON.stringify(initialJson))
+      .then(() => {
+        console.log("API Started and file has been created!");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+});
--- routes/accounts.js
+++ routes/accounts.js
@@ -8,9 +8,18 @@ const router = express.Router();
 router.post("/", async (req, resp) => {
   try {
     let account = req.body;
+
+    if (!account.name || account.balance == null) {
+      throw new Error("Name e Balance são obrigatórios.");
+    }
+
     const data = JSON.parse(await readFile(global.fileName));
 
-    account = { id: data.nextId++, ...account };
+    account = {
+      id: data.nextId++,
+      name: account.name,
+      balance: account.balance,
+    };
     data.accounts.push(account);
 
     await writeFile(global.fileName, JSON.stringify(data, null, 2));
@@ -26,9 +35,88 @@ router.get("/", async (req, resp) => {
     const data = JSON.parse(await readFile(global.fileName));
     delete data.nextId;
     resp.send(data);
+    console.log("Dados retornados");
   } catch (err) {
     resp.status(400).send({ error: err.message });
   }
+});
+
+router.get("/:id", async (req, resp) => {
+  try {
+    const data = JSON.parse(await readFile(global.fileName));
+    const account = data.accounts.find(
+      (account) => account.id === parseInt(req.params.id)
+    );
+    resp.send(account);
+  } catch (error) {
+    resp.status(400).send({ error: err.message });
+  }
+});
+
+router.delete("/:id", async (req, resp) => {
+  try {
+    const data = JSON.parse(await readFile(global.fileName));
+    data.accounts = data.accounts.filter(
+      (account) => account.id !== parseInt(req.params.id)
+    );
+    await writeFile(global.fileName, JSON.stringify(data, null, 2));
+    resp.end();
+  } catch (error) {
+    resp.status(400).send({ error: err.message });
+  }
+});
+
+router.put("/", async (req, resp) => {
+  try {
+    const account = req.body;
+
+    if (!account.name || account.balance == null) {
+      throw new Error("Name e Balance são obrigatórios.");
+    }
+
+    const data = JSON.parse(await readFile(global.fileName));
+    const index = data.accounts.findIndex((a) => a.id === parseInt(account.id));
+
+    if(index === -1){
+      throw new Error("Registro não encontrado.")
+    }
+
+    data.accounts[index].name = account.name;
+    data.accounts[index].balance = account.balance;
+
+    await writeFile(global.fileName, JSON.stringify(data, null, 2));
+    resp.send(account);
+  } catch (error) {
+    resp.status(400).send({ error: err.message });
+  }
+});
+
+router.patch("/updateBalance", async (req, resp) => {
+  try {
+    const account = req.body;
+
+    if (!account.id || account.balance == null) {
+      throw new Error("ID e Balance são obrigatórios.");
+    }
+
+    const data = JSON.parse(await readFile(global.fileName));
+    const index = data.accounts.findIndex((a) => a.id === parseInt(account.id));
+
+    if(index === -1){
+      throw new Error("Registro não encontrado.")
+    }
+
+    data.accounts[index].balance = account.balance;
+    await writeFile(global.fileName, JSON.stringify(data, null, 2));
+    resp.send(data.accounts[index]);
+  } catch (error) {
+    resp.status(400).send({ error: err.message });
+  }
+});
+
+router.use((err, req, resp, next) => {
+  console.log(err);
+  resp.status(400).send({ error: err.message });
 });
 
 export default router;
